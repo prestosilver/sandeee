17c83219c8981944b3123c331fc577d0ce6a91cb
bob16795 <prestosilver.ptp@gmail.com>
2023-04-20 06:21:47 -0500

    add more tests

--- build.zig
+++ build.zig
@@ -47,11 +47,11 @@ pub fn emails(b: *std.build.Builder, path: []const u8) []const u8 {
 }
 
 const asmTestsFiles = [_][]const u8{ "hello", "window", "texture", "fib", "arraytest", "audiotest", "tabletest", "send", "recv" };
-const eonTestsFiles = [_][]const u8{ "fib", "tabletest", "heaptest" };
+const eonTestsFiles = [_][]const u8{ "fib", "tabletest", "heaptest", "stringtest" };
 const asmExecFiles = [_][]const u8{ "dump", "echo", "aplay", "libdump" };
-const eonExecFiles = [_][]const u8{ "asm", "pix", "connectris" };
+const eonExecFiles = [_][]const u8{ "updater", "asm", "pix", "connectris" };
 const asmLibFiles = [_][]const u8{ "string", "window", "texture", "sound", "array" };
-const eonLibFiles = [_][]const u8{ "heap", "table" };
+const eonLibFiles = [_][]const u8{ "ui", "heap", "table" };
 const wavSoundFiles = [_][]const u8{ "login", "message" };
 const pngImageFiles = [_][]const u8{ "bar", "editor", "email", "explorer", "window", "web", "wall", "barlogo", "cursor", "scroll", "connectris" };
 const internalImageFiles = [_][]const u8{ "logo", "load", "sad", "bios" };
