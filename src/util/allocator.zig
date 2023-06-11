19d2112a9a5c02d0fe5b45d7484e0cccd917a51f
bob16795 <prestosilver.ptp@gmail.com>
2023-06-11 09:41:35 -0500

    reenable clib

--- src/util/allocator.zig
+++ src/util/allocator.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 const builtin = @import("builtin");
-pub const useclib = false;
+pub const useclib = true;
 
 pub var gpa = std.heap.GeneralPurposeAllocator(.{ .stack_trace_frames = 10 }){};
 pub const alloc = if (builtin.is_test) std.testing.allocator else if (!builtin.link_libc or !useclib) gpa.allocator() else std.heap.c_allocator;
