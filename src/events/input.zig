ad9def7ff566215034df2606b98e7275a4e2cdef
bob16795 <prestosilver.ptp@gmail.com>
2023-06-14 14:01:14 -0500

    switch to cammel case

--- src/events/input.zig
+++ src/events/input.zig
@@ -4,12 +4,12 @@ const c = @import("../c.zig");
 
 pub fn setup(win: ?*c.GLFWwindow, enabled: bool) void {
     if (enabled) {
-        _ = c.glfwSetCursorPosCallback(win, cursor_pos_callback);
-        _ = c.glfwSetKeyCallback(win, key_callback);
-        _ = c.glfwSetCharCallback(win, char_callback);
-        _ = c.glfwSetMouseButtonCallback(win, mouse_button_callback);
-        _ = c.glfwSetFramebufferSizeCallback(win, framebuffer_size_callback);
-        _ = c.glfwSetScrollCallback(win, scroll_callback);
+        _ = c.glfwSetCursorPosCallback(win, cursorPosCallback);
+        _ = c.glfwSetKeyCallback(win, keyCallback);
+        _ = c.glfwSetCharCallback(win, charCallback);
+        _ = c.glfwSetMouseButtonCallback(win, mouseButtonCallback);
+        _ = c.glfwSetFramebufferSizeCallback(win, framebufferSizeCallback);
+        _ = c.glfwSetScrollCallback(win, scrollCallback);
     } else {
         _ = c.glfwSetCursorPosCallback(win, null);
         _ = c.glfwSetKeyCallback(win, null);
@@ -31,15 +31,15 @@ pub const EventKeyChar = struct { codepoint: u32, mods: i32 };
 
 var global_mods: i32 = 0;
 
-pub fn cursor_pos_callback(_: ?*c.GLFWwindow, x: f64, y: f64) callconv(.C) void {
+pub fn cursorPosCallback(_: ?*c.GLFWwindow, x: f64, y: f64) callconv(.C) void {
     ev.EventManager.instance.sendEvent(EventMouseMove{ .x = x, .y = y });
 }
 
-pub fn char_callback(_: ?*c.GLFWwindow, codepoint: c_uint) callconv(.C) void {
+pub fn charCallback(_: ?*c.GLFWwindow, codepoint: c_uint) callconv(.C) void {
     ev.EventManager.instance.sendEvent(EventKeyChar{ .codepoint = codepoint, .mods = global_mods });
 }
 
-pub fn key_callback(_: ?*c.GLFWwindow, key: c_int, _: c_int, action: c_int, mods: c_int) callconv(.C) void {
+pub fn keyCallback(_: ?*c.GLFWwindow, key: c_int, _: c_int, action: c_int, mods: c_int) callconv(.C) void {
     switch (action) {
         c.GLFW_PRESS => ev.EventManager.instance.sendEvent(EventKeyDown{ .key = key, .mods = mods }),
         c.GLFW_REPEAT => ev.EventManager.instance.sendEvent(EventKeyDown{ .key = key, .mods = mods }),
@@ -50,7 +50,7 @@ pub fn key_callback(_: ?*c.GLFWwindow, key: c_int, _: c_int, action: c_int, mods
     global_mods = mods;
 }
 
-pub fn mouse_button_callback(_: ?*c.GLFWwindow, btn: c_int, action: c_int, _: c_int) callconv(.C) void {
+pub fn mouseButtonCallback(_: ?*c.GLFWwindow, btn: c_int, action: c_int, _: c_int) callconv(.C) void {
     switch (action) {
         c.GLFW_PRESS => ev.EventManager.instance.sendEvent(EventMouseDown{ .btn = btn }),
         c.GLFW_RELEASE => ev.EventManager.instance.sendEvent(EventMouseUp{ .btn = btn }),
@@ -58,10 +58,10 @@ pub fn mouse_button_callback(_: ?*c.GLFWwindow, btn: c_int, action: c_int, _: c_
     }
 }
 
-pub fn framebuffer_size_callback(_: ?*c.GLFWwindow, width: c_int, height: c_int) callconv(.C) void {
+pub fn framebufferSizeCallback(_: ?*c.GLFWwindow, width: c_int, height: c_int) callconv(.C) void {
     ev.EventManager.instance.sendEvent(EventWindowResize{ .w = width, .h = height });
 }
 
-pub fn scroll_callback(_: ?*c.GLFWwindow, x: f64, y: f64) callconv(.C) void {
+pub fn scrollCallback(_: ?*c.GLFWwindow, x: f64, y: f64) callconv(.C) void {
     ev.EventManager.instance.sendEvent(EventMouseScroll{ .x = @floatCast(f32, x), .y = @floatCast(f32, y) });
 }
