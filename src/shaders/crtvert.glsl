91c55570a9c368dcc4d477433ce60123548bd7f6
bob16795 <prestosilver.ptp@gmail.com>
2023-08-06 14:43:41 -0500

    shader updates

--- src/shaders/crtvert.glsl
+++ src/shaders/crtvert.glsl
@@ -1,10 +1,13 @@
 #version 330 core
 layout (location = 0) in vec3 aVertex;
 
+uniform float screen_width = 1024;
+uniform float screen_height = 600;
+
 out vec3 SCREEN_UV;
 
 void main()
 {
-    gl_Position = vec4(aVertex.x * 2560, aVertex.y * 1080, 0.0 , 1.0);
+    gl_Position = vec4(aVertex.x * screen_width, aVertex.y * screen_height, 0.0 , 1.0);
     SCREEN_UV = gl_Position.xyz;
 }
