285efed5cc0fabcf4271bf7b08d69433cbf8fd74
bob16795 <prestosilver.ptp@gmail.com>
2025-05-27 09:06:42 -0500

    Fix some assembly bugs

--- content/asm/exec/aplay.asm
+++ content/asm/exec/aplay.asm
@@ -4,20 +4,22 @@ main:
     copy 0
     jz error                    ; jump if zero
     sys 3                       ; open file
-    copy 0                       ; duplacte file handle
-    push 100000000              ; read size
+    copy 0                      ; duplacte file handle
+    push 1
+    sys 8                       ; get file arg
+    sys 19                      ; get size of file 
     sys 4                       ; read
     call play_sound
     sys 1
 
 play_sound:
-    push "/fake/snd/play"       ; cont path
-    sys 3                       ; cont handle
-    copy 0                       ; cont handle handle
-    copy 2                       ; cont handle handle cont
-    sys 5                       ; cont handle
-    sys 7                       ; cont
-    disc 0                      ;
+    push "/fake/snd/play"      ; cont path
+    sys 3                      ; cont handle
+    dup 0                      ; cont handle handle
+    dup 2                      ; cont handle handle cont
+    sys 5                      ; cont handle
+    sys 7                      ; cont
+    disc 0                     ;
     ret
 
 error:
