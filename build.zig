c1bd933321ca2c4022828bc46a46d135468a2357
prestosilver <prestosilver.ptp@gmail.com>
2025-12-16 10:56:24 -0500

    meta fix dependency order

--- build.zig
+++ build.zig
@@ -740,10 +740,10 @@ pub fn build(b: *std.Build) !void {
     });
 
     const run_exe_tests = b.addRunArtifact(exe_tests);
+    run_exe_tests.dependOn(disk_step);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_tests.step);
-    test_step.dependOn(disk_step);
 
     // public builds step
     const pub_step = b.step("pub", "Build all public builds");
